fc0191c477e1429779a2e3d819ec7721e893b746
Lucas Anjos <lucaslahp77@gmail.com>
2026-05-11 11:45:34 -0300

    Dependencias

---
+++ backend/refugee-help/src/main/java/com/imperium/arhelp/refugee_help/RefugeeHelpApplication.java
@@ -0,0 +1,13 @@
+package com.imperium.arhelp.refugee_help;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefugeeHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefugeeHelpApplication.class, args);
+	}
+
+}
---
+++ backend/refugee-help/src/test/java/com/imperium/arhelp/refugee_help/RefugeeHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imperium.arhelp.refugee_help;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefugeeHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
